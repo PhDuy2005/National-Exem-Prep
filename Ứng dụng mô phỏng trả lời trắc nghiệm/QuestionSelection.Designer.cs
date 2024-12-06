426cd7cdc28b3c30fa88bc3493380ed0d14adc91
Khanh Duy <94819005+PhDuy2005@users.noreply.github.com>
2024-12-06 23:50:01 +0700

    FE: Giao diện chọn câu hỏi

---
+++ Ứng dụng mô phỏng trả lời trắc nghiệm/QuestionSelection.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Ứng_dụng_mô_phỏng_trả_lời_trắc_nghiệm
+{
+    partial class QuestionSelection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_TimeLeft = new System.Windows.Forms.Label();
+            this.lb_clock = new System.Windows.Forms.Label();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_TimeLeft
+            // 
+            this.lb_TimeLeft.AutoSize = true;
+            this.lb_TimeLeft.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.lb_TimeLeft.ForeColor = System.Drawing.Color.Red;
+            this.lb_TimeLeft.Location = new System.Drawing.Point(12, 9);
+            this.lb_TimeLeft.Name = "lb_TimeLeft";
+            this.lb_TimeLeft.Size = new System.Drawing.Size(208, 35);
+            this.lb_TimeLeft.TabIndex = 0;
+            this.lb_TimeLeft.Text = "Thời gian còn lại";
+            this.lb_TimeLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lb_clock
+            // 
+            this.lb_clock.AutoSize = true;
+            this.lb_clock.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.lb_clock.ForeColor = System.Drawing.Color.Red;
+            this.lb_clock.Location = new System.Drawing.Point(226, 9);
+            this.lb_clock.Name = "lb_clock";
+            this.lb_clock.Size = new System.Drawing.Size(78, 35);
+            this.lb_clock.TabIndex = 1;
+            this.lb_clock.Text = "00:00";
+            this.lb_clock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn_submit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btn_submit.Location = new System.Drawing.Point(662, 9);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(126, 52);
+            this.btn_submit.TabIndex = 7;
+            this.btn_submit.Text = "Nộp bài";
+            this.btn_submit.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(770, 352);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // QuestionSelection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.lb_clock);
+            this.Controls.Add(this.lb_TimeLeft);
+            this.Name = "QuestionSelection";
+            this.Text = "QuestionSelection";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_TimeLeft;
+        private System.Windows.Forms.Label lb_clock;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
